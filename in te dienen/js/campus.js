63409a54dc93db953c59ddbb06b3a0b6cdf874b8
TVke <thomas.verhelst@student.kdg.be>
2016-11-06 23:59:09 +0100

    added delivered solution fontEnd

--- in te dienen/js/campus.js
+++ in te dienen/js/campus.js
@@ -1,5 +1,5 @@
 // kaart
-document.querySelector("#bovenkant h3").addEventListener("click",function() {
+document.querySelector(".bovenkant h3").addEventListener("click",function() {
 	document.getElementById("controller").classList.toggle("openMap");
 });
 
@@ -7,7 +7,7 @@ document.querySelector("#bovenkant h3").addEventListener("click",function() {
 function toon(classname) {
 	document.getElementById("controller").classList.toggle(classname);
 }
-var bovenBlocks = document.querySelectorAll("#bovenkant div[id^='blok-']");
+var bovenBlocks = document.querySelectorAll(".bovenkant div[class^='blok-']");
 
 for(let i=0,ilen=bovenBlocks.length;i<ilen;++i) {
 	bovenBlocks[i].addEventListener("mouseover", function (){toon("toon");});
@@ -15,7 +15,7 @@ for(let i=0,ilen=bovenBlocks.length;i<ilen;++i) {
 }
 
 // onderblok
-var onderBlocks = document.querySelectorAll("#onderkant div[id^='blok-']");
+var onderBlocks = document.querySelectorAll(".onderkant div[class^='blok-']");
 
 for(let i=0,ilen=onderBlocks.length;i<ilen;++i) {
 	onderBlocks[i].addEventListener("mouseover", function (){toon("toononder");});
@@ -23,12 +23,13 @@ for(let i=0,ilen=onderBlocks.length;i<ilen;++i) {
 }
 
 // testbutton
-document.querySelector("header button").addEventListener("click",function () {
-	var links = document.querySelectorAll("#hyperlinks p");
-	for(let i=0,ilen=links.length;i<ilen;++i){
+document.querySelector("header button").addEventListener("click",testButton);
+function testButton() {
+	var links = document.querySelectorAll(".hyperlinks p");
+	for (let i = 0, ilen = links.length; i < ilen; ++i) {
 		links[i].classList.toggle("hidden");
 	}
-});
+}
 
 // glow function
 
@@ -44,16 +45,15 @@ var glow = function(element,time=500) {
 };
 
 var hyperlinks=[];
-hyperlinks[0] = document.querySelectorAll("#hyperlinks p:first-child a");
-hyperlinks[1] = document.querySelectorAll("#hyperlinks p:nth-child(2) a");
-hyperlinks[2] = document.querySelectorAll("#hyperlinks p:last-child a");
+hyperlinks[0] = document.querySelectorAll(".hyperlinks p:first-child a");
+hyperlinks[1] = document.querySelectorAll(".hyperlinks p:last-child a");
 
 hyperlinks[0][0].addEventListener("click",function () {
-	glow(document.getElementById("bovenkant"));
-	glow(document.getElementById("onderkant"));
+	glow(document.getElementsByClassName("bovenkant")[0]);
+	glow(document.getElementsByClassName("onderkant")[0]);
 });
 hyperlinks[0][1].addEventListener("click",function () {
-	var wholeBlock = document.querySelectorAll("#blok-C, #blok-C div[class^='verdieping-']");
+	var wholeBlock = document.querySelectorAll(".blok-C, .blok-C div[class^='verdieping-']");
 	for(let i=0,ilen=wholeBlock.length;i<ilen;++i){
 		glow(wholeBlock[i]);
 	}
@@ -61,17 +61,17 @@ hyperlinks[0][1].addEventListener("click",function () {
 hyperlinks[0][2].addEventListener("click",function () {
 	toon("toon");
 	setTimeout(function () {
-		document.querySelector("#blok-C .verdieping-4").classList.add("hidden");
-		document.querySelector("#blok-C").classList.add("hide");
+		document.querySelector(".blok-C .verdieping-4").classList.add("hidden");
+		document.querySelector(".blok-C").classList.add("hide");
 	},500);
 	setTimeout(function () {
-		document.querySelector("#blok-C .verdieping-3").classList.add("hidden");
+		document.querySelector(".blok-C .verdieping-3").classList.add("hidden");
 	},1000);
 	setTimeout(function () {
-		document.querySelector("#blok-C .verdieping-2").classList.add("hidden");
+		document.querySelector(".blok-C .verdieping-2").classList.add("hidden");
 	},1500);
 	setTimeout(function () {
-		glow(document.querySelector("#blok-C .verdieping-1"));
+		glow(document.querySelector(".blok-C .verdieping-1"));
 	},2500);
 	setTimeout(function () {
 		document.querySelector(".verdieping-2").classList.remove("hidden");
@@ -81,27 +81,26 @@ hyperlinks[0][2].addEventListener("click",function () {
 	},4000);
 	setTimeout(function () {
 		document.querySelector(".verdieping-4").classList.remove("hidden");
-		document.querySelector("#blok-C").classList.remove("hide");
+		document.querySelector(".blok-C").classList.remove("hide");
 	},4500);
 	setTimeout(function () {
 		toon("toon");
 	},5000);
 });
 hyperlinks[0][3].addEventListener("click",function () {
-	//glow();
 	toon("zoom");
 	setTimeout(function () {
-		document.querySelector("#blok-C .verdieping-4").classList.add("hidden");
-		document.querySelector("#blok-C").classList.add("hide");
+		document.querySelector(".blok-C .verdieping-4").classList.add("hidden");
+		document.querySelector(".blok-C").classList.add("hide");
 	},500);
 	setTimeout(function () {
-		document.querySelector("#blok-C .verdieping-3").classList.add("hidden");
+		document.querySelector(".blok-C .verdieping-3").classList.add("hidden");
 	},1000);
 	setTimeout(function () {
-		document.querySelector("#blok-C .verdieping-2").classList.add("hidden");
+		document.querySelector(".blok-C .verdieping-2").classList.add("hidden");
 	},1500);
 	setTimeout(function () {
-		glow(document.querySelector("#blok-C .verdieping-1 .lokaal-06"),1000);
+		glow(document.querySelector(".blok-C .verdieping-1 .lokaal-06"),1000);
 	},2500);
 	setTimeout(function () {
 		document.querySelector(".verdieping-2").classList.remove("hidden");
@@ -111,9 +110,50 @@ hyperlinks[0][3].addEventListener("click",function () {
 	},6000);
 	setTimeout(function () {
 		document.querySelector(".verdieping-4").classList.remove("hidden");
-		document.querySelector("#blok-C").classList.remove("hide");
+		document.querySelector(".blok-C").classList.remove("hide");
 	},6500);
 	setTimeout(function () {
 		toon("zoom");
 	},7000);
+});
+hyperlinks[1][0].addEventListener("click",function () {
+	toon("toon");
+	setTimeout(function () {
+		toon("links");
+		testButton();
+	},500);
+	setTimeout(function () {
+		toon("toon");
+	},1000);
+});
+hyperlinks[0][4].addEventListener("click",function () {
+	glow(document.getElementsByClassName("bovenkant")[1]);
+	glow(document.getElementsByClassName("onderkant")[1]);
+});
+hyperlinks[0][5].addEventListener("click",function () {
+	toon("zoom");
+	setTimeout(function () {
+		document.querySelector("#antwerpen .blok-B .verdieping-3").classList.add("hidden");
+		document.querySelector("#antwerpen .blok-B").classList.add("hide");
+	},500);
+	setTimeout(function () {
+		glow(document.querySelector("#antwerpen .blok-B .verdieping-2 .lokaal-06"),1000);
+	},1000);
+	setTimeout(function () {
+		document.querySelector("#antwerpen .blok-B .verdieping-3").classList.remove("hidden");
+		document.querySelector("#antwerpen .blok-B").classList.remove("hide");
+	},3000);
+	setTimeout(function () {
+		toon("zoom");
+	},4500);
+});
+hyperlinks[1][1].addEventListener("click",function () {
+	toon("toon");
+	setTimeout(function () {
+		toon("links");
+		testButton();
+	},500);
+	setTimeout(function () {
+		toon("toon");
+	},1000);
 });
